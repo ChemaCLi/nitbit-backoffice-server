bfc3971f2df018f64ae54c3258d13274030219a0
José María CL <chema9703@gmail.com>
2025-02-03 02:29:54 -0600

    Setup swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express'
 import placesRoutes from './modules/places/infrastructure/routes'
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const swaggerUi = require('swagger-ui-express')
+import * as openapi from './docs/openapi.json' // Adjust the path as needed
 
 const app = express()
 app.use(express.json())
-app.use(placesRoutes)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapi))
+app.use('/places', placesRoutes)
 
 app.listen(3000, () => {
   console.info('Server running on http://localhost:3000')
