3f9b8d7ad0076ad30f88d3f6ca34639d06d92a8e
José María CL <chema9703@gmail.com>
2025-02-04 15:47:33 -0600

    Update comments

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,21 @@ import express from 'express'
 import placesRoutes from './modules/places/infrastructure/routes'
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const swaggerUi = require('swagger-ui-express')
-import * as openapi from './docs/openapi.json' // Adjust the path as needed
+import * as openapi from './docs/openapi.json'
 
 import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes'
-
-const theme = new SwaggerTheme()
-const darkStyle = theme.getBuffer(SwaggerThemeNameEnum.DRACULA) // Getting a Style
-
 const app = express()
 app.use(express.json())
 
+// swagger auto generated docs
+const theme = new SwaggerTheme()
+const darkStyle = theme.getBuffer(SwaggerThemeNameEnum.DRACULA)
 const options = {
   explorer: true,
-  customCss: darkStyle, // Apply the 'dark' theme
+  customCss: darkStyle,
 }
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapi, options))
+
 app.use('/places', placesRoutes)
 
 app.listen(3000, () => {
