20db862ed03f0f7f2623f4ee92f47e2c59069c8c
Jose Maria CLi <chema9703@gmail.com>
2025-02-10 00:12:11 -0600

    chore: Update the config file

--- src/modules/shared/application/config.ts
+++ src/modules/shared/application/config.ts
@@ -1,8 +1,8 @@
 const {
   REST_API_PORT = 3000,
+  SYSTEM_EMAIL = 'noreply@noreply.com',
   RESEND_API_KEY = '',
   JWT_SECRET = 'default_secret',
-  SYSTEM_EMAIL = 'onboarding@resend.dev',
 } = process.env
 
 export const config = Object.freeze({
